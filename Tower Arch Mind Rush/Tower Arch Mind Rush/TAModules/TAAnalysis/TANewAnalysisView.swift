93be8240947ddf67219ed3b1a1c797e123737a40
Dias <dias.atudinov@gmail.com>
2026-03-03 02:30:42 +0500

    new files

---
+++ Tower Arch Mind Rush/Tower Arch Mind Rush/TAModules/TAAnalysis/TANewAnalysisView.swift
@@ -0,0 +1,18 @@
+//
+//  TANewAnalysisView.swift
+//  Tower Arch Mind Rush
+//
+//  Created by Dias Atudinov on 02.03.2026.
+//
+
+import SwiftUI
+
+struct TANewAnalysisView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TANewAnalysisView()
+}
